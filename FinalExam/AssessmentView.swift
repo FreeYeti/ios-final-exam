985daa06070a8e2f62891d038d7bd3f29c84f3c4
Jack Li <liujin834@gmail.com>
2022-08-20 09:10:16 -0400

    add assessment view

--- FinalExam/AssessmentView.swift
+++ FinalExam/AssessmentView.swift
@@ -8,11 +8,29 @@
 import UIKit
 
 class AssessmentView: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var yesBtn: UIButton!
+    @IBOutlet weak var noBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // use default color
         view.backgroundColor = hexStringToUIColor(hex: CONFIG_MAIN_BGCOLOR)
+        
+        resultLabel.text = ""
+    }
+    
+    @IBAction func clickYes(){
+        resultLabel.text = "Please get tested for COVID-19."
+        yesBtn.isEnabled = false
+        noBtn.isEnabled = false
+    }
+    
+    @IBAction func clickNo(){
+        resultLabel.text = "You are ok, please don't worry."
+        yesBtn.isEnabled = false
+        noBtn.isEnabled = false
     }
 }
